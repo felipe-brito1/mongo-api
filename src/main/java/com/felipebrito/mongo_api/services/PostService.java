b67253d858b093afdab197b9295dddf8f39a5d4b
Felipe Brito <felipebrito0429@gmail.com>
2026-03-23 00:18:25 -0300

    Retrieving post by ID

---
+++ src/main/java/com/felipebrito/mongo_api/services/PostService.java
@@ -0,0 +1,23 @@
+package com.felipebrito.mongo_api.services;
+
+import com.felipebrito.mongo_api.domain.Post;
+import com.felipebrito.mongo_api.domain.User;
+import com.felipebrito.mongo_api.repository.PostRepository;
+import com.felipebrito.mongo_api.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id){
+        Optional<Post> obj = repo.findById(id);
+    return obj.orElseThrow(()-> new ObjectNotFoundException("Object not found!"));
+    }
+
+}
