68acfbdf65c8774f71a67239ce9a996d88e7a2a2
Felipe Brito <felipebrito0429@gmail.com>
2026-03-23 14:26:48 -0300

    Simple query using query methods

--- src/main/java/com/felipebrito/mongo_api/resources/PostResource.java
+++ src/main/java/com/felipebrito/mongo_api/resources/PostResource.java
@@ -3,6 +3,7 @@ package com.felipebrito.mongo_api.resources;
 import com.felipebrito.mongo_api.domain.Post;
 import com.felipebrito.mongo_api.domain.User;
 import com.felipebrito.mongo_api.dto.UserDTO;
+import com.felipebrito.mongo_api.resources.util.URL;
 import com.felipebrito.mongo_api.services.PostService;
 import com.felipebrito.mongo_api.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,12 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id){
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 }
